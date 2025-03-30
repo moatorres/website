59723023f3286362a30d3c1350677dea3db2e75f
Moa Torres <44585769+moatorres@users.noreply.github.com>
2025-03-30 13:12:47 -0300

    setup(blog): setup nextjs blog app

---
+++ apps/blog/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  displayName: 'blog',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: 'test-output/jest/coverage',
+}
+
+export default config
---
+++ apps/blog/next.config.mjs
@@ -0,0 +1,68 @@
+import { composePlugins, withNx } from '@nx/next'
+
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+
+Object.assign(process.env, {
+  NEXT_TELEMETRY_DISABLED: '1',
+})
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  cleanDistDir: true,
+  env: {
+    NEXT_TELEMETRY_DISABLED: '1',
+  },
+  experimental: {
+    viewTransition: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  nx: {
+    svgr: false,
+  },
+  pageExtensions: ['mdx', 'tsx'],
+  poweredByHeader: false,
+  reactStrictMode: true,
+}
+
+const withMdx = createMDX({
+  extension: /\.(md|mdx)?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'wrap',
+          properties: {
+            className: ['decoration-[none]'],
+          },
+        },
+      ],
+    ],
+  },
+})
+
+const withPlugins = composePlugins(withNx, withMdx)
+
+export default withPlugins(nextConfig)
