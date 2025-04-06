2e0e57d9104a24dd0cdda4b07af50805e7647d29
Moa Torres <44585769+moatorres@users.noreply.github.com>
2025-04-06 20:08:33 -0300

    chore(blog): enabled underline on anchors and improved syntax highlight contrast

--- apps/blog/src/mdx-components.tsx
+++ apps/blog/src/mdx-components.tsx
@@ -50,7 +50,7 @@ const components = {
   ),
   a: ({ href, children, ...props }: ComponentPropsWithoutRef<'a'>) => {
     const className =
-      'text-gray-600 hover:text-gray-800 dark:text-gray-400 hover:dark:text-gray-300 dark:underline dark:underline-offset-2 dark:decoration-gray-800 no-underline transition-all duration-50'
+      'text-gray-600 hover:text-gray-800 dark:text-gray-400 hover:dark:text-gray-300 underline underline-offset-2 dark:underline dark:underline-offset-2 dark:decoration-gray-800 transition-all duration-50'
 
     if (href?.startsWith('/')) {
       return (
