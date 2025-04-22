0f5e28a685866f302d6bb4f8bf7e5e00793c0d62
Moa Torres <44585769+moatorres@users.noreply.github.com>
2025-04-22 18:35:49 -0300

    feat(blog): added rss route

---
+++ apps/blog/src/app/rss/route.ts
@@ -0,0 +1,38 @@
+import articles from '@/data/articles.json'
+import config from '@/data/config.json'
+
+export async function GET() {
+  const itemsXml = articles
+    .sort((a, b) => {
+      if (new Date(a.date) > new Date(b.date)) {
+        return -1
+      }
+      return 1
+    })
+    .map(
+      (post) =>
+        `<item>
+          <title>${post.title}</title>
+          <link>${config.previewUrl + post.href}</link>
+          <description>${post.summary || ''}</description>
+          <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+        </item>`
+    )
+    .join('\n')
+
+  const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
+  <rss version="2.0">
+    <channel>
+        <title>${config.title}</title>
+        <link>${config.previewUrl}</link>
+        <description>This is ${config.author}'s RSS feed</description>
+        ${itemsXml}
+    </channel>
+  </rss>`
+
+  return new Response(rssFeed, {
+    headers: {
+      'Content-Type': 'application/rss+xml',
+    },
+  })
+}
