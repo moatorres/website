322be4412ad77bf85bf2143693ee7ce4f05b89c6
Moa Torres <44585769+moatorres@users.noreply.github.com>
2025-04-29 02:17:53 -0300

    feat(blog): added speed insights and updated dependencies

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -4,6 +4,7 @@
  */
 
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import LocalFont from 'next/font/local'
 
@@ -86,6 +87,7 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
             {children}
             <Footer />
             <Analytics />
+            <SpeedInsights />
           </Page>
         </ThemeProvider>
       </body>
