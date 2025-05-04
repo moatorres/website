f6c91a129fab8cedb146c3dbf098e6d76e8e7ae9
Moa Torres <44585769+moatorres@users.noreply.github.com>
2025-05-04 10:32:19 -0300

    fix(blog): updated error pages layout

--- apps/blog/src/app/error.tsx
+++ apps/blog/src/app/error.tsx
@@ -10,6 +10,7 @@ import Link from 'next/link'
 import React from 'react'
 
 import { Button } from '@/components/button'
+import { Page } from '@/components/page'
 
 type ErrorProps = {
   error: Error & { digest?: string }
@@ -18,27 +19,29 @@ type ErrorProps = {
 
 export default function Error({ error, reset }: ErrorProps) {
   return (
-    <div className="flex items-center min-h-[75vh] px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
-      <div className="w-full space-y-6 text-center">
-        <div className="space-y-3">
-          <h1 className="text-3xl md:text-5xl font-bold tracking-tighter">
-            Looks like our servers took an unexpected coffee break. ☕️
-          </h1>
-          <pre>500 Internal Server Error</pre>
-          <div className="flex space-x-3 justify-center">
-            <Button asChild variant="soft">
-              <Link href="/">
-                <ArrowLeft />
-                Home
-              </Link>
-            </Button>
-            <Button variant="soft" onClick={() => reset()}>
-              <RefreshCw />
-              Reload
-            </Button>
+    <Page>
+      <div className="flex items-center min-h-[75vh] px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+        <div className="w-full space-y-6 text-center">
+          <div className="space-y-3">
+            <h1 className="text-3xl md:text-5xl font-bold tracking-tighter">
+              Looks like our servers took an unexpected coffee break. ☕️
+            </h1>
+            <pre>500 Internal Server Error</pre>
+            <div className="flex space-x-3 justify-center">
+              <Button asChild variant="soft">
+                <Link href="/">
+                  <ArrowLeft />
+                  Home
+                </Link>
+              </Button>
+              <Button variant="soft" onClick={() => reset()}>
+                <RefreshCw />
+                Reload
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Page>
   )
 }
