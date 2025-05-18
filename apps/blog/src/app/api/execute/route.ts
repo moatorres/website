2c86ea9c0ba036302590add8efe2643cce33dfa5
Moa Torres <44585769+moatorres@users.noreply.github.com>
2025-05-18 03:01:38 -0300

    fix(blog): use dirname outside .next directory

--- apps/blog/src/app/api/execute/route.ts
+++ apps/blog/src/app/api/execute/route.ts
@@ -53,7 +53,7 @@ export async function POST(req: NextRequest) {
 
   const filePath = resolve(
     __dirname,
-    `../../../../.next/snippets/${parsed.data.name}.js`
+    `../../../../snippets/${parsed.data.name}.js`
   )
 
   const stream = new ReadableStream({
