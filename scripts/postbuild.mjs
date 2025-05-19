f8fadb82ca2bcbdd8db9f2484ba8070c3c29e7d9
Moa Torres <44585769+moatorres@users.noreply.github.com>
2025-05-19 07:37:34 -0300

    chore(repo): removed postbuild script

--- scripts/postbuild.mjs
+++
@@ -1,7 +0,0 @@
-import { cp } from 'fs/promises'
-import { join } from 'path'
-
-const src = join(process.cwd(), 'apps/blog/public/_snippets')
-const dst = join(process.cwd(), 'apps/blog/.next/snippets')
-
-await cp(src, dst, { recursive: true })
